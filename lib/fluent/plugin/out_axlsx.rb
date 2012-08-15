3db97d4a0a2a66a590862bdd3a08c1f7674f7381
MATSUMOTO Katsuyoshi <matsumoto.katsuyoshi+github@gmail.com>
2012-08-16 00:53:54 +0900

    work emit for excel worksheets

--- lib/fluent/plugin/out_axlsx.rb
+++ lib/fluent/plugin/out_axlsx.rb
@@ -3,14 +3,18 @@ class AxlsxOutput < Output
   Plugin.register_output('axlsx', self)
 
   config_param :path, :string
+  config_param :worksheets, :string, :default => "main"
 
   def initialize
     super
     require 'axlsx'
+    @xlsx = Axlsx::Package.new
+    @workbook = @xlsx.workbook
   end
 
   def configure(conf)
     super
+    @worksheet = @workbook.add_worksheet(:name => @worksheets.to_s)
   end
 
   def start
@@ -20,6 +24,14 @@ class AxlsxOutput < Output
   end
 
   def format(tag, time, record)
+  end
+
+  def emit(tag, es, chunk)
+    es.each do |time, record|
+      row = record.values
+      @worksheet.add_row(row)
+    end
+    @xlsx.serialize(path)
   end
 end
 end
